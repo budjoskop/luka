b0f120ee56511884ddacb9b41a0f390cbc7b3525
Ognjen Tomic / Diwanee <ognjen.tomic@diwanee.com>
2019-05-09 15:22:09 +0200

    timer

--- counter.js
+++ counter.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("March 14, 2018 10:00:00").getTime();
+var countDownDate = new Date("May 16, 2019 18:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
