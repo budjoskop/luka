6d0ff1ae596d671f220c113f3ecc98bf6bef462a
Ognjen Tomic / Diwanee <ognjen.tomic@diwanee.com>
2019-06-21 11:35:29 +0200

    komtati

--- quiz-1.js
+++ quiz-1.js
@@ -1,30 +1,37 @@
-$(document).ready(function()
-{    $("#check").click(function() {
+$(document).ready(function () {
+  $("#check").click(function () {
 
-if (!$("input[@name=q1]:checked").val()
+    if (!$("input[@name=q1]:checked").val()
 
-) {
-alert("You're not done yet!");
-}
+    ) {
+      alert("You're not done yet!");
+    } else {
 
-else {
+      function eraseText() {
+        document.getElementById("check").value = "";
+      }
 
-  function eraseText() {
-    document.getElementById("check").value = "";
-}
+      // ovde se menja kviz
 
-// ovde se menja kviz
-
-var cat1 = ($("input[@name=q1]:checked").val() != "c");
-var cat2 = ($("input[@name=q1]:checked").val() == "c");
+      var cat1 = ($("input[@name=q1]:checked").val() != "a");
+      var cat2 = ($("input[@name=q1]:checked").val() == "a");
 
 
 
 
-if (cat1) { $("#category1").show("slow");$("#category2").hide("slow"); };
-if (cat2) { $("#category2").show("slow"); $("#category1").hide("slow"); };
+      if (cat1) {
+        $("#category1").show("slow");
+        $("#category2").hide("slow");
+      };
+      if (cat2) {
+        $("#category2").show("slow");
+        $("#category1").hide("slow");
+      };
 
 
-{ $("#closing").show("slow"); };
-}
-    });});
+      {
+        $("#closing").show("slow");
+      };
+    }
+  });
+});
